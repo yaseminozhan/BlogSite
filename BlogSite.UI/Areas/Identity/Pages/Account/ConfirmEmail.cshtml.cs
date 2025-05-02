c4a30918507219d91ab33dc8a59fea1b9f1fcc93
yaseminozhan <yasemiinozhan@gmail.com>
2025-05-02 20:59:32 +0300

    some editing

--- BlogSite.UI/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ BlogSite.UI/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using System;
+﻿using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,12 +20,9 @@ namespace BlogSite.UI.Areas.Identity.Pages.Account
             _userManager = userManager;
         }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [TempData]
         public string StatusMessage { get; set; }
+
         public async Task<IActionResult> OnGetAsync(string userId, string code)
         {
             if (userId == null || code == null)
@@ -45,7 +38,14 @@ namespace BlogSite.UI.Areas.Identity.Pages.Account
 
             code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
             var result = await _userManager.ConfirmEmailAsync(user, code);
-            StatusMessage = result.Succeeded ? "Thank you for confirming your email." : "Error confirming your email.";
+
+            if (result.Succeeded)
+            {
+                TempData["ConfirmMessage"] = "E-posta adresiniz başarıyla doğrulandı. Giriş yapabilirsiniz.";
+                return RedirectToPage("/Account/Login");
+            }
+
+            StatusMessage = "Error confirming your email.";
             return Page();
         }
     }
